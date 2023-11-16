648503a1806037da145b4872ff9afc5ec64c9d00
Rekxis <rekxis@gmx.de>
2023-11-16 12:56:52 +0100

    refactored brettAnzeigen, added first draft of moving pieces and updated main

--- SchachEinfach.java
+++ SchachEinfach.java
@@ -1,19 +1,19 @@
+import java.util.Scanner;
+
 public class SchachEinfach {
     private static char[][] spielBrett = new char[10][10];
+    private final static String[] spieler = {"Schwarz", "Weiß"};
+    private final static char dunkel = '\u2593'; // unicode für eine dunkle fläche
+    private final static char hell = '\u2591'; // unicode für eine helle fläche
+    private static int zugNr = 1;
 
     private static char[][] brettAufbauen() {
-        char dunkel = '\u2593'; // unicode für eine dunkle fläche
-        char hell = '\u2591'; // unicode für eine helle fläche
         for (int zeile = 0; zeile < spielBrett.length; zeile++) {
             for (int spalte = 0; spalte < spielBrett[zeile].length; spalte++) {
                 switch (zeile) {
                     case 0:
                     case 9:
-                        if (spalte >= 1 && spalte <= 8) {
-                            spielBrett[zeile][spalte] = (char) (64 + spalte);
-                        } else {
-                            spielBrett[zeile][spalte] = ' ';
-                        }
+                        spielBrett[zeile][spalte] = (char) (spalte >= 1 && spalte <= 8 ? 64 + spalte : 32);
                         break;
                     default:
                         if (spalte >= 1 && spalte <= 8) {
@@ -21,39 +21,21 @@ public class SchachEinfach {
                         } else {
                             spielBrett[zeile][spalte] = (char) (57 - zeile);
                         }
-                        break;
                 }
             }
         }
         return spielBrett;
     }
 
-    private static void brettAnzeigen() {
+    private static void brettDarstellen(boolean richtung) {
         for (int zeile = 0; zeile < spielBrett.length; zeile++) {
             for (int spalte = 0; spalte < spielBrett[zeile].length; spalte++) {
-                System.out.print(spielBrett[zeile][spalte]);
+                int z = (richtung) ? zeile : (spielBrett.length - 1 - zeile);
+                int s = (richtung) ? spalte : (spielBrett.length - 1 - spalte);
+                System.out.print(spielBrett[z][s]);
             }
             System.out.println();
         }
-    }
-
-    private static void brettDrehen(char[][] spielBrett) {
-        int groesse = spielBrett.length;
-        char[][] gedrehtesBrett = new char[groesse][groesse];
-
-        for (int zeile = 0; zeile < groesse; zeile++) {
-            for (int spalte = 0; spalte < groesse; spalte++) {
-                gedrehtesBrett[zeile][spalte] = spielBrett[groesse - 1 - zeile][groesse - 1 - spalte];
-            }
-        }
-
-        for (int zeile = 0; zeile < groesse; zeile++) {
-            for (int spalte = 0; spalte < groesse; spalte++) {
-                spielBrett[zeile][spalte] = gedrehtesBrett[zeile][spalte];
-            }
-        }
-        System.out.println("Gedrehtes Spielbrett:");
-
     }
 
     private static void figurenSetzen() {
@@ -67,11 +49,36 @@ public class SchachEinfach {
         }  
     }
 
+    private static void figurBewegen(boolean wer) {
+        System.out.println((wer ? spieler[0] : spieler[1]) + " ist am Zug");
+        Scanner sc = new Scanner(System.in);
+        String eingabe = sc.nextLine();
+        int zeileVon, spalteVon, zeileNach, spalteNach;
+
+        zeileVon =  9 - eingabe.charAt(1) + 48;  
+        spalteVon =     eingabe.charAt(0) - 64; 
+        zeileNach = 9 - eingabe.charAt(3) + 48;
+        spalteNach =    eingabe.charAt(2) - 64;
+
+        spielBrett[zeileNach][spalteNach] = spielBrett[zeileVon][spalteVon];
+        spielBrett[zeileVon][spalteVon] = ((zeileVon + spalteVon) % 2 == 0) ? hell : dunkel;
+    }
+
+
+    public static int getZugNr() {
+        return zugNr;
+    }
+
     public static void main(String[] args) {
         spielBrett = brettAufbauen();
         figurenSetzen();
-        brettAnzeigen();
-        brettDrehen(spielBrett);
-        brettAnzeigen();
+
+        do {
+            brettDarstellen(true);
+            figurBewegen(getZugNr() % 2 == 0);
+            System.out.println("\n\n\n\n");
+            brettDarstellen(false);
+            figurBewegen(getZugNr() % 2 == 0);
+        } while (getZugNr() < 6);
     }
 }
